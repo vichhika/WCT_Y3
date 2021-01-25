7cb5e1ae2fef0f89f04531ebee2df4ca6348f307
Leang <ngounmengleang18@gmail.com>
2021-01-25 16:40:05 +0700

    cpu display all

---
+++ tamplet/script/Build.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+        cpu(); 
+})
+
+function cpu(){
+    
+    $.getJSON( "./../../Data/Chantra.json", (data) => {
+        for(cpu of data.cpu) {
+            $('#pcConponent').append(
+                '<tr id='+cpu.id+'>'+
+                    "<th><span>CPU</span></th>"+
+                    '<td><span id="">'+cpu.product+'</span></td>'+
+                    '<td><span id="">'+cpu.price+'</span></td>'+
+                    '<td><button class="btn btn-primary px-5" type="submit">Add</button></td>'+
+                '</tr>'
+            )
+        }
+    })
+}
--- tamplet/script/HomeScript.js
+++ tamplet/script/HomeScript.js
@@ -1,5 +1,7 @@
+
 $(document).ready(function(){
     $('#startBtn').click(()=>{
         location.href = "./../html/build.html"
     })
+   
 })
