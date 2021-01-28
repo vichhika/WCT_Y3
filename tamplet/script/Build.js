9bfdade0e64f39c1bcc32ebf700907e191c2119f
Sambath <dilytithsambath@gmail.com>
2021-01-28 15:57:44 +0700

    Revert "done next buuton"
    This reverts commit 59e46cbe4fc83a82f41a3c0dc43b33559af47ec7.

--- tamplet/script/Build.js
+++ tamplet/script/Build.js
@@ -2,27 +2,15 @@ $(document).ready(function(){
     displayCon(); 
 })
 
-function nextCon(){
-    $('#pcConponent').empty();
-    displayCon();
-}
-
 function displayCon(){
     
-    $.getJSON( "./../../Data/Chantra.json", (data) => {  
-        let currentConponent = {'name':"",'setCon':""}
-         currentConponent.name =  $('#conponentTitle').text();
-        switch(currentConponent.name){
-            default : {currentConponent.name = "CPU",currentConponent.setCon = data.chantra.cpu}  break
-            case 'CPU' : {currentConponent.name = "RAM",currentConponent.setCon = data.chantra.ram} break
-            case 'RAM' : {currentConponent.name = "Harddisk",currentConponent.setCon = data.chantra.harddisk} break
-        }
-        $('#conponentTitle').text(currentConponent.name);
-        for(con of currentConponent.setCon) {
+    $.getJSON( "./../../Data/Chantra.json", (data) => {
+        //$('#conponentTitle').text(data)
+        for(con of data.cpu) {
             $('#pcConponent').append(
 <<<<<<< HEAD
                 '<tr id='+con.id+'>'+
-                    "<th><span>"+currentConponent.name+"</span></th>"+
+                    "<th><span>"+con+"</span></th>"+
                     '<td><span id="">'+con.product+'</span></td>'+
                     '<td><span id="">'+con.price+'</span></td>'+
 =======
