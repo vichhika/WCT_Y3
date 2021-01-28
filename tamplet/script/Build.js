94667762f21a8e46bfa98b32c59423363ec36a20
Ngoun Mengleang <ngounmengleang18@gmail.com>
2021-01-28 16:08:35 +0700

    re push

--- tamplet/script/Build.js
+++ tamplet/script/Build.js
@@ -2,23 +2,28 @@ $(document).ready(function(){
     displayCon(); 
 })
 
+function nextCon(){
+    $('#pcConponent').empty();
+    displayCon();
+}
+
 function displayCon(){
     
-    $.getJSON( "./../../Data/Chantra.json", (data) => {
-        //$('#conponentTitle').text(data)
-        for(con of data.cpu) {
+    $.getJSON( "./../../Data/Chantra.json", (data) => {  
+        let currentConponent = {'name':"",'setCon':""}
+         currentConponent.name =  $('#conponentTitle').text();
+        switch(currentConponent.name){
+            default : {currentConponent.name = "CPU",currentConponent.setCon = data.chantra.cpu}  break
+            case 'CPU' : {currentConponent.name = "RAM",currentConponent.setCon = data.chantra.ram} break
+            case 'RAM' : {currentConponent.name = "Harddisk",currentConponent.setCon = data.chantra.harddisk} break
+        }
+        $('#conponentTitle').text(currentConponent.name);
+        for(con of currentConponent.setCon) {
             $('#pcConponent').append(
-<<<<<<< HEAD
                 '<tr id='+con.id+'>'+
-                    "<th><span>"+con+"</span></th>"+
+                    "<th><span>"+currentConponent.name+"</span></th>"+
                     '<td><span id="">'+con.product+'</span></td>'+
                     '<td><span id="">'+con.price+'</span></td>'+
-=======
-                '<tr id='+cpu.id+'>'+
-                    "<th><span>CPU</span></th>"+
-                    '<td><span id="">'+cpu.product+'</span></td>'+
-                    '<td><span id="">'+cpu.price+'</span></td>'+
->>>>>>> parent of 37002b0... edit some ui on build html
                     '<td><button class="btn btn-primary px-5" type="submit">Add</button></td>'+
                 '</tr>'
             )
