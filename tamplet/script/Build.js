93fb3f9ee5b4a20bd8a01982fff309aa78cfaca5
Lymeng <Lymengchun@gmail.com>
2021-01-29 12:03:53 +0700

    merge all

--- tamplet/script/Build.js
+++ tamplet/script/Build.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-    displayCon(); 
-})
-
-function nextCon(){
-    $('#pcConponent').empty();
-    displayCon();
-}
-
-
-
-function displayCon(){
-    
-    $.getJSON( "./../../Data/Chantra.json", (data) => {  
-        let currentConponent = {'name':"",'setCon':""}
-         currentConponent.name =  $('#conponentTitle').text();
-        switch(currentConponent.name){
-            default : {currentConponent.name = "CPU",currentConponent.setCon = data.chantra.cpu}  break
-            case 'CPU' : {currentConponent.name = "RAM",currentConponent.setCon = data.chantra.ram} break
-            case 'RAM' : {currentConponent.name = "Harddisk",currentConponent.setCon = data.chantra.harddisk} break
-        }
-        $('#conponentTitle').text(currentConponent.name);
-        for(con of currentConponent.setCon) {
-            $('#pcConponent').append(
-                '<tr id='+con.id+'>'+
-                    "<th><span>"+currentConponent.name+"</span></th>"+
-                    '<td><span id="">'+con.product+'</span></td>'+
-                    '<td><span id="">'+con.price+'</span></td>'+
-                    '<td><button class="btn btn-primary px-5" type="submit">Add</button></td>'+
-                '</tr>'
-            )
-        }
-    })
-}
-=======
 var mybuild = [];
 var id = 0;
 $(document).ready(function(){
@@ -111,4 +75,3 @@ function add(Comp,Model,Price){
 
 
 
->>>>>>> main
