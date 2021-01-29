4f4b977896527c6a45b832cd5b1e4ccc99c4c89a
Ngoun Mengleang <ngounmengleang18@gmail.com>
2021-01-29 11:05:42 +0700

    Revert "add pc shop"
    This reverts commit 3638f6d8b2877fdac9c053e58ecd6223d3dd26db.
    
    Conflicts:
    	tamplet/html/build.html
    	tamplet/script/Build.js

--- tamplet/script/Build.js
+++ tamplet/script/Build.js
@@ -1,15 +1,65 @@
 var mybuild = [];
 
 $(document).ready(function(){
-    displayCon(); 
+    displayShop()
 })
 
-function nextCon(){
-    $('#pcConponent').empty();
-    displayCon();
+//by leang
+let currentConponent = {'shop':null , 'name':null,'setCon':null}
+let displayShop = () =>{
+    $('#tableTitle').append('<th scope="col">Shop</th><th scope="col">Facebook</th><th scope="col"></th>')
+    $.getJSON( "./../../Data/Chantra.json", (data) => {  
+        for(con in data) {
+            $('#pcConponent').append(
+                '<tr id='+con+'>'+
+                    '<th><span>'+con+'</span></th>'+
+                    '<th><span>Facebook</span></th>'+
+                    '<td><button class="btn btn-primary px-5 addShop" onClick="shopMenu(this)" type="submit">chose</button></td>'+
+                '</tr>'
+            )
+        }
+    })
 }
 
-<<<<<<< HEAD
+//by leang
+let shopMenu = (me) => {
+    let shopName = $(me).parent().parent().attr('id')
+    $.getJSON( "./../../Data/Chantra.json", (data) => {  
+        switch (shopName){
+            case 'Chantra' : {currentConponent.shop = data.Chantra}  break
+            case 'GoldOne': {currentConponent.shop = data.GoldOne}  break
+            case 'Vtech' : {currentConponent.shop = data.Vtech}  break
+            case 'TK': {currentConponent.shop = data.TK}  break
+        }
+    })
+}
+
+//by leang
+$(".btnNextShop").click((me) => {
+    $(me.target).removeClass("btnNextShop").addClass("btnNext")
+    displayCon();
+})
+
+//improve code by leang
+$(".btnNext").click(displayCon = () => {
+    $('#pcConponent').empty();
+    switch(currentConponent.name){
+    default : {currentConponent.name = "CPU",currentConponent.setCon = currentConponent.shop.cpu}  break
+    case 'CPU' : {currentConponent.name = "RAM",currentConponent.setCon = currentConponent.shop.ram} break
+    case 'RAM' : {currentConponent.name = "Storage",currentConponent.setCon = currentConponent.shop.harddisk} break
+    }
+    $('#conponentTitle').text(currentConponent.name);
+    for(con of currentConponent.setCon) {
+        $('#pcConponent').append(
+            '<tr id='+con.id+'>'+
+                "<th><span>"+currentConponent.name+"</span></th>"+
+                '<td><span>'+con.product+'</span></td>'+
+                '<td><span>'+con.price+'</span></td>'+
+                `<td><button id=${"addBtn"+con.id} class="btn btn-primary px-5" type="button" onclick="add('${currentConponent.name}','${con.product}','${con.price}')">Add</button></td>`+            '</tr>'
+        )
+    }
+});
+
 function add(Comp,Model,Price){
     /*declare object to hold the data info*/
     var component = {comp:"",model:"",price:""}
@@ -20,36 +70,5 @@ function add(Comp,Model,Price){
     sessionStorage.setItem("myBuild",JSON.stringify(mybuild));
 }
 
-=======
->>>>>>> parent of 3638f6d... add pc shop
-function displayCon(){
-    
-    $.getJSON( "./../../Data/Chantra.json", (data) => {  
-        let currentConponent = {'name':"",'setCon':""}
-         currentConponent.name =  $('#conponentTitle').text();
-        switch(currentConponent.name){
-            default : {currentConponent.name = "CPU",currentConponent.setCon = data.chantra.cpu}  break
-            case 'CPU' : {currentConponent.name = "RAM",currentConponent.setCon = data.chantra.ram} break
-            case 'RAM' : {currentConponent.name = "Harddisk",currentConponent.setCon = data.chantra.harddisk} break
-        }
-        $('#conponentTitle').text(currentConponent.name);
-<<<<<<< HEAD
-        for(con of currentConponent.setContype="submit") {
-=======
-        for(con of currentConponent.setCon) {
->>>>>>> parent of 3638f6d... add pc shop
-            $('#pcConponent').append(
-                '<tr id='+con.id+'>'+
-                    "<th><span>"+currentConponent.name+"</span></th>"+
-                    '<td><span id="">'+con.product+'</span></td>'+
-                    '<td><span id="">'+con.price+'</span></td>'+
-<<<<<<< HEAD
-                    `<td><button id=${"addBtn"+con.id} class="btn btn-primary px-5" type="button" onclick="add('${currentConponent.name}','${con.product}','${con.price}')">Add</button></td>`+
-=======
-                    '<td><button class="btn btn-primary px-5" type="submit">Add</button></td>'+
->>>>>>> parent of 3638f6d... add pc shop
-                '</tr>'
-            )
-        }
-    })
-}
+
+
