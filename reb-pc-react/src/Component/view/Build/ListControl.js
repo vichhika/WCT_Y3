b14bb4defb2ac9d812ec75ced608767d7d5c74f1
Ngoun Mengleang <ngounmengleang18@gmail.com>
2021-03-19 15:39:19 +0700

    Haft way to convert to componetn

---
+++ reb-pc-react/src/Component/view/Build/ListControl.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function ListControl() {
+    return (
+        <div class="btn-group" role="group" aria-label="First group">
+            <button type="button" class="btn btn-outline-secondary active">
+                1</button>
+            <button type="button" class="btn btn-outline-secondary">2</button>
+            <button type="button" class="btn btn-outline-secondary">3</button>
+            <button type="button" class="btn btn-outline-secondary">4</button>
+            <button type="button" class="btn btn-outline-secondary" disabled>
+                ...</button>
+            <button type="button" class="btn btn-outline-secondary">
+                <i class="fal fa-long-arrow-right"></i>
+            </button>
+        </div>
+    )
+}
