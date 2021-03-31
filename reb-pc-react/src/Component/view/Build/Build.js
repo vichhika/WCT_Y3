8097e4b2df77d99faa629abbaa41265f8d47a649
Ngoun Mengleang <ngounmengleang18@gmail.com>
2021-03-31 08:51:31 +0700

    process bar

--- reb-pc-react/src/Component/view/Build/Build.js
+++ reb-pc-react/src/Component/view/Build/Build.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./../../../Css/build.scss";
-import "./ProductList"
 import ProductList from "./ProductList";
+import ProcessBar from "./ProcessBar";
 
 function Build() {
   return (
@@ -11,130 +11,8 @@ function Build() {
         <p class="card-text mt-3">
           Complete all the step below to successful build your own PC.
         </p>
-
-        <div class="progressBar d-flex flex-row justify-content-center mt-3">
-          
-          <img
-            src="https://www.flaticon.com/svg/vstatic/svg/911/911514.svg?token=exp=1616753708~hmac=6e3c22309b57e3769b3afd1000cb8cbc"
-            alt="CPU"
-            style={{borderColor: "green"}}
-          />
-          <div class="progress">
-            <div
-              class="progress-bar bg-success"
-              role="progressbar"
-              style={{ width: "100%" }}
-              aria-valuenow="100"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-
-          <img
-            src="https://img.icons8.com/carbon-copy/2x/motherboard.png"
-            alt="Motherboard"
-          />
-          <div class="progress">
-            <div
-              class="progress-bar bg-success"
-              role="progressbar"
-              style={{ width: "50%" }}
-              aria-valuenow="50"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-
-          <img
-            src="https://cdn0.iconfinder.com/data/icons/mobile-phone-componets-1/144/mobile-icon_05-512.png"
-            alt="RAM"
-          />
-
-          <div class="progress">
-            <div
-              class="progress-bar bg-success"
-              role="progressbar"
-              style={{ width: "0%" }}
-              aria-valuenow="0"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-
-          <img
-            src="https://findicons.com/files/icons/1580/devine_icons_part_2/512/hdd_2.png"
-            alt="Storage"
-          />
-
-          <div class="progress">
-            <div
-              class="progress-bar bg-success"
-              role="progressbar"
-              style={{ width: "0%" }}
-              aria-valuenow="0"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-
-          <img
-            src="https://cdn0.iconfinder.com/data/icons/computer-interface-glyph/128/1-10-512.png"
-            alt="VGA"
-          />
-
-          <div class="progress">
-            <div
-              class="progress-bar bg-success"
-              role="progressbar"
-              style={{ width: "0%" }}
-              aria-valuenow="0"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-
-          <img
-            src="https://cdn0.iconfinder.com/data/icons/pc-hardware-2/128/PC_Hardware_Set_Artboard_15-512.png"
-            alt="PowerSupply"
-          />
-
-          <div class="progress">
-            <div
-              class="progress-bar bg-success"
-              role="progressbar"
-              style={{ width: "0%" }}
-              aria-valuenow="0"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-
-          <img
-            src="https://static.thenounproject.com/png/1875372-200.png"
-            alt="Case"
-          />
-
-          <div class="progress">
-            <div
-              class="progress-bar bg-success"
-              role="progressbar"
-              style={{ width: "0%" }}
-              aria-valuenow="0"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-
-          <img
-            src="https://img.pngio.com/computer-screen-icon-png-239841-free-icons-library-computer-monitor-icon-png-1600_1600.jpg"
-            alt="CPU"
-          />
-
-        </div>
-
+        <ProcessBar />
         <ProductList />
-
-
       </div>
     </div>
   );
---
+++ reb-pc-react/src/Component/view/Build/ProcessBar.js
@@ -0,0 +1,55 @@
+import React, { useContext } from 'react';
+import { buildContext } from "./../../Context/BuildContext";
+import cpu from './../../../img/componentImg/cpu.png';
+import motherboard from './../../../img/componentImg/motherboard.png';
+import ram from './../../../img/componentImg/ram.webp';
+import hdd from './../../../img/componentImg/hdd.png';
+import gpu from './../../../img/componentImg/gpu.webp';
+import casePC from './../../../img/componentImg/case.png';
+import power from './../../../img/componentImg/power.webp';
+import monitor from './../../../img/componentImg/monitor.jpg'
+
+
+function ProcessBar() {
+    const { contextState } = useContext(buildContext)
+    const componentImg = [cpu, motherboard, ram, hdd, gpu, casePC, power, monitor];
+    const process = () => {
+        let item = []
+        for (let i = 0; i < 8; i++) {
+            let processStyle = { borderColor: "#ccc" };
+            let barStyle = { width: "0" }
+            if (i <= contextState.component) {
+                processStyle.borderColor = "green";
+            }
+            item.push(<><img
+                src={componentImg[i]}
+                style={processStyle}
+            /></>)
+            if (i < contextState.component) {
+                barStyle.width = "100%"
+            }
+            if (i !== 7) {
+                item.push(<><div class="progress">
+                    <div
+                        class="progress-bar bg-success"
+                        role="progressbar"
+                        style={barStyle}
+                        aria-valuenow="100"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                    ></div>
+                </div></>)
+            }
+        }
+        return item;
+    }
+    return (
+        <div>
+            <div class="progressBar d-flex flex-row justify-content-center mt-3">
+                {process()}
+            </div>
+        </div>
+    )
+}
+
+export default ProcessBar
