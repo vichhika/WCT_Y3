ebc2b8839b757738b072f8a7655b01eda279f49f
Lymeng <lymengchun@gmail.com>
2021-04-01 11:53:44 +0700

    added LoginPage

---
+++ reb-pc-react/src/Component/view/LoginAndSignUp/SignUp.js
@@ -0,0 +1,50 @@
+import React from "react";
+import  "./../../../Css/stylesignup.scss";
+
+function SignUp() {
+    return(
+        <div class="container">
+       
+        <div class="row">
+            <div class="col-sm">
+
+                
+                <div class="card" style={{width:"20rem"}}>
+                    <form>
+                        <h1>Sign Up</h1>
+                        <div class="form-group" >
+                            <label for="InputSurname">Username</label>
+                            <input type="text" class="form-control" id="InputSurname" placeholder="Enter Username" required></input>
+                        </div>
+                        <div class="form-group" >
+                            <label for="InputEmail">Email address</label>
+                            <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="Enter email" required></input>
+                            
+                        </div>
+                        <div class="form-group">
+                            <label for="InputPassword">Password</label>
+                            <input type="password" class="form-control" id="InputPassword" placeholder="Enter Password" required></input>
+                        </div>
+                        <div class="form-group">
+                            <label for="ConfirmPassword">Confirm Password</label>
+                            <input type="password" class="form-control" id="ConfirmPassword" placeholder="Confirm Password" required></input>
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="Check1"></input>
+                            <label class="form-check-label" for="Check1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Sign Up</button>
+                        
+                        
+                    </form>
+                </div>
+            </div>
+
+           
+
+        </div>
+
+    </div>
+    );
+}
+export default SignUp;
