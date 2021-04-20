e98b59703ad58ac13e3de72cd811ee4e034010cc
Lymeng <lymengchun@gmail.com>
2021-04-20 15:36:32 +0700

    donate

--- reb-pc-react/src/Component/view/donate.js
+++ reb-pc-react/src/Component/view/donate.js
@@ -1,6 +1,8 @@
 import React from "react";
-
+import ABA from './../../img/aba.png'
+import './../../Css/donate.scss'
 function Donate() {
+
   return (
     <div className="jumbotron jumbotron-fluid d-flex align-content-center my-0">
     <div className="container  my-auto content">
@@ -11,9 +13,10 @@ function Donate() {
           Donate us
         </button>
       </p>
+
       <div className="collapse " id="collapseExample">
         <div className="card card-body ">
-            <img src="./../img/aba.png" style={{width:'265px;',height:'50%;'}}></img>
+            <img src={ABA}></img>
             <p>XXXXXXXXXXX</p>
             <p>Thank you</p>
         </div>
