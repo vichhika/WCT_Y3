c5f301a9e7c9e66d09fce37af7f61e44547b7a64
Vichhika <vichhikapann@gmail.com>
2021-06-04 16:47:27 +0700

    store product build and query relative build

--- web_api/app/Http/Controllers/BuildpcController.php
+++ web_api/app/Http/Controllers/BuildpcController.php
@@ -187,15 +187,16 @@ class BuildpcController extends Controller
  * @OA\RequestBody(
  *    required=true,
  *    @OA\JsonContent(
- *       required={"cpupriceID","casepcpriceID","internalharddrivepriceID","memorypriceID","monitorpriceID","motherboardpriceID","powersupplypriceID","videocardpriceID"},
- *      @OA\Property(property="cpupriceID", type="Integer", format="Integer", example="1"),
- *  *      @OA\Property(property="casepcpriceID", type="Integer", format="Integer", example="1"),
+ *       required={"adminshopID","cpuID","casepcID","internalharddriveID","memoryID","monitorID","motherboardID","powersupplyID","videocardID"},
+ *      @OA\Property(property="adminshopID", type="Integer", format="Integer", example="1"),
+ *          @OA\Property(property="cpuID", type="Integer", format="Integer", example="1"),
+ *  *      @OA\Property(property="casepcID", type="Integer", format="Integer", example="1"),
  *  *      @OA\Property(property="internalharddriveID", type="Integer", format="Integer", example="1"),
- *  *      @OA\Property(property="memorypriceID", type="Integer", format="Integer", example="1"),
- *  *      @OA\Property(property="monitorpriceID", type="Integer", format="Integer", example="1"),
- *  *      @OA\Property(property="motherboardpriceID", type="Integer", format="Integer", example="1"),
- *  *      @OA\Property(property="powersupplypriceID", type="Integer", format="Integer", example="1"),
- *  *      @OA\Property(property="videocardpriceID", type="Integer", format="Integer", example="1"),
+ *  *      @OA\Property(property="memoryID", type="Integer", format="Integer", example="1"),
+ *  *      @OA\Property(property="monitorID", type="Integer", format="Integer", example="1"),
+ *  *      @OA\Property(property="motherboardID", type="Integer", format="Integer", example="1"),
+ *  *      @OA\Property(property="powersupplyID", type="Integer", format="Integer", example="1"),
+ *  *      @OA\Property(property="videocardID", type="Integer", format="Integer", example="1"),
  *    ),
  * ),
  * @OA\Response(
@@ -211,25 +212,50 @@ class BuildpcController extends Controller
     public function save(Request $request)
     {
         $request->validate([
-            'cpupriceID' => 'required|numeric',
-            'motherboardpriceID' => 'required|numeric',
-            'powersupplypriceID' => 'required|numeric',
-            'internalharddrivepriceID' => 'required|numeric',
-            'monitorpriceID' => 'required|numeric',
-            'videocardpriceID' => 'required|numeric',
-            'casepcpriceID' => 'required|numeric',
-            'memorypriceID' => 'required|numeric',
+            'adminshopID' => 'required|numeric',
+            'cpuID' => 'required|numeric',
+            'casepcID' => 'required|numeric',
+            'internalharddriveID' => 'required|numeric',
+            'memoryID' => 'required|numeric',
+            'monitorID' => 'required|numeric',
+            'motherboardID' => 'required|numeric',
+            'powersupplyID' => 'required|numeric',
+            'videocardID' => 'required|numeric',
         ]);
 
         $components = array(
-            Cpuprice::where('cpupriceID', $request->cpupriceID)->first(),
-            Caseprice::where('casepcpriceID', $request->casepcpriceID)->first(),
-            Internalharddriveprice::where('internalharddrivepriceID', $request->internalharddrivepriceID)->first(),
-            Memoryprice::where('memorypriceID', $request->memorypriceID)->first(),
-            Monitorprice::where('monitorpriceID', $request->monitorpriceID)->first(),
-            Motherboardprice::where('motherboardpriceID', $request->motherboardpriceID)->first(),
-            Powersupplyprice::where('powersupplypriceID', $request->powersupplypriceID)->first(),
-            Videocardprice::where('videocardpriceID', $request->videocardpriceID)->first()
+            Cpuprice::where([
+                ['adminshopID' , $request->adminshopID],
+                ['cpuID' , $request->cpuID]
+            ])->first(),
+            Caseprice::where([
+                ['adminshopID' , $request->adminshopID],
+                ['casepcID' , $request->casepcID]
+            ])->first(),
+            Internalharddriveprice::where([
+                ['adminshopID' , $request->adminshopID],
+                ['internalharddriveID' , $request->internalharddriveID]
+            ])->first(),
+            Memoryprice::where([
+                ['adminshopID' , $request->adminshopID],
+                ['memoryID' , $request->memoryID]
+            ])->first(),
+            Monitorprice::where([
+                ['adminshopID' , $request->adminshopID],
+                ['monitorID' , $request->monitorID]
+            ])->first(),
+            Motherboardprice::where([
+                ['adminshopID' , $request->adminshopID],
+                ['motherboardID' , $request->motherboardID]
+            ])->first(),
+            Powersupplyprice::where([
+                ['adminshopID' , $request->adminshopID],
+                ['powersupplyID' , $request->powersupplyID]
+            ])->first(),
+            Videocardprice::where([
+                ['adminshopID' , $request->adminshopID],
+                ['videocardID' , $request->videocardID]
+            ])->first(),
         );
 
         foreach($components as $is_exist){
@@ -237,19 +263,19 @@ class BuildpcController extends Controller
                 return response()->json([
                     'statusCode' => 0,
                     'message' => 'invalid submit.'
-                ],400);
+                ]);
             }
         }
 
         Productbuild::create([
-            'cpupriceID' => $request->cpupriceID,
-            'motherboardpriceID' => $request->motherboardpriceID,
-            'powersupplypriceID' => $request->powersupplypriceID,
-            'internalharddrivepriceID' => $request->internalharddrivepriceID,
-            'monitorpriceID' => $request->internalharddrivepriceID,
-            'videocardpriceID' => $request->videocardpriceID,
-            'casepcpriceID' => $request->casepcpriceID,
-            'memorypriceID' =>$request->memorypriceID,
+            'cpupriceID' => $components[0]->cpupriceID,
+            'casepcpriceID' => $components[1]->casepcpriceID,
+            'internalharddrivepriceID' => $components[2]->internalharddrivepriceID,
+            'memorypriceID' =>$components[3]->memorypriceID,
+            'monitorpriceID' => $components[4]->monitorpriceID,
+            'motherboardpriceID' => $components[5]->motherboardpriceID,
+            'powersupplypriceID' => $components[6]->powersupplypriceID,
+            'videocardpriceID' => $components[7]->videocardpriceID,
             'id' => $request->user()->id,
         ]);
 
@@ -258,6 +284,54 @@ class BuildpcController extends Controller
             'message' => 'save successfully.'
         ],201);
 
+    }
+
+    /**
+ * @OA\Post(
+ * path="/api/build/delete",
+ * summary="user delete build product",
+ * tags={"user"},
+ * security={ {"sanctum": {} }},
+ * @OA\RequestBody(
+ *    required=true,
+ *    @OA\JsonContent(
+ *       required={"productbuildID"},
+ *      @OA\Property(property="productbuildID", type="Integer", format="Integer", example="1"),
+ *    ),
+ * ),
+ * @OA\Response(
+ *    response=200,
+ *    description="",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="please test it")
+ *        )
+ *     )
+ * )
+ */
+
+    public function destory(Request $request)
+    {
+        $request->validate([
+            'productbuildID' => 'required|numeric',
+        ]);
+
+        $productbuild = Productbuild::where([
+            ['productbuildID',$request->productbuildID],
+            ['id',$request->user()->id]
+        ])->first();
+
+        if(!$productbuild)
+        {
+            return response()->json([
+                'statusCode' => 0,
+                'message' => 'product is not existed.'
+            ]);
+        }
+        $productbuild->delete();
+        return response()->json([
+            'statusCode' => 1,
+            'message' => 'product deleted successfully.'
+        ]);
     }
 
     /**
@@ -352,14 +426,15 @@ class BuildpcController extends Controller
                 $totalprice = $cpuprice->price + $casepcprice->price + $internalharddriveprice->price + $memoryprice->price + $monitorprice->price + $motherboardprice->price + $powersupplyprice->price + $videocardprice->price;
                 array_push($result,array(
                 'adminshopID' => $shop->adminshopID,
-                'cpupriceID' => $cpuprice->cpupriceID,
-                'motherboardpriceID' => $motherboardprice->motherboardpriceID,
-                'powersupplypriceID' => $powersupplyprice->powersupplypriceID,
-                'internalharddrivepriceID' => $internalharddriveprice->internalharddrivepriceID,
-                'monitorpriceID' => $monitorprice->monitorpriceID,
-                'videocardpriceID' => $videocardprice->videocardpriceID,
-                'casepcpriceID' => $casepcprice->casepcpriceID,
-                'memorypriceID' =>$memoryprice->memorypriceID,
+                'shop_name' => $shop->shop_name,
+                // 'cpupriceID' => $cpuprice->cpupriceID,
+                // 'motherboardpriceID' => $motherboardprice->motherboardpriceID,
+                // 'powersupplypriceID' => $powersupplyprice->powersupplypriceID,
+                // 'internalharddrivepriceID' => $internalharddriveprice->internalharddrivepriceID,
+                // 'monitorpriceID' => $monitorprice->monitorpriceID,
+                // 'videocardpriceID' => $videocardprice->videocardpriceID,
+                // 'casepcpriceID' => $casepcprice->casepcpriceID,
+                // 'memorypriceID' =>$memoryprice->memorypriceID,
                 'totalprice' => $totalprice,
                ));
             }
